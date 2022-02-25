bdc6d6a3ae3b6e4c53e7dced56068e9d5e8ec319
Tomivan <gracejunkie20@gmail.com>
2022-02-25 16:15:42 +0100

    firebase and stripe updated

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51KWinbFlFJCXTJaawWd7JzAwnh7Hjxn5eKMxKMZ81pI0xbjktvQSiUoPC6e2sFALIxTZWF1PobeuZyVELopeZrBu003R2nftV9';
 
   const onToken = token => {
     console.log(token);
@@ -13,7 +13,7 @@ const StripeCheckoutButton = ({ price }) => {
   return (
     <StripeCheckout
       label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      name='CROWN Clothing Ltd.'
       billingAddress
       shippingAddress
       image='https://svgshare.com/i/CUz.svg'
